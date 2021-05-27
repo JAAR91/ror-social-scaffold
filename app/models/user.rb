d571377bfb52314005dbc810081c269f65627a52
JAAR91 <jaarriagaramos@gmail.com>
2021-05-27 17:15:05 -0600

    add inverted_friendships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :friendships, foreign_key: :user_id, dependent: :destroy
+  has_many :inverted_friendships, class_name: "Friendship", foreign_key: "friend_id"
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
